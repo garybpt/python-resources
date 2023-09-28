f13573b0707ad6ae2ca6c8eed59db827b414b12c
Gary Butterfield <gary.butterfield@joyjunction.co.uk>
2023-09-28 22:48:05 +0100

    Did some json testing plus completed lesson 8 task 1.

---
+++ chatgpt-python-course/practice-sessions/l8-2a-lists.py
@@ -0,0 +1,32 @@
+# Starting list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Append 6 to the end of the list
+numbers.append(6)
+for number in numbers:
+    print(numbers)
+
+# Insert 0 at the beginning of the list
+numbers.insert(0, 0)
+for number in numbers:
+    print(numbers)
+
+# Remove 3 from the list
+numbers.remove(3)
+for number in numbers:
+    print(numbers)
+
+# Extend the list with 7, 8, and 9
+numbers.extend([7, 8, 9])
+for number in numbers:
+    print(numbers)
+
+# Sort the list in ascending order
+numbers.sort()
+for number in numbers:
+    print(numbers)
+
+# Sort the list in descending order
+numbers.reverse()
+for number in numbers:
+    print(numbers)

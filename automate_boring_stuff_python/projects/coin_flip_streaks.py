59867d43e83efdb6e3b80e558479e1962c547c33
Gary Butterfield <gary.butterfield@joyjunction.co.uk>
2024-01-07 17:54:14 +0000

    Completed comma_code.py project and set up for the remaining two.

---
+++ automate_boring_stuff_python/projects/coin_flip_streaks.py
@@ -0,0 +1,12 @@
+import random
+
+number_of_streaks = 0
+
+for experiment_number in range(10000):
+    # Code that creates a list of 100 'heads' or 'tails' values.
+
+
+    # Code that checks if there is a streak of 6 heads or tails in a row.
+
+
+print('Chance of streak: %s%%' % (number_of_streaks / 100))
---
+++ automate_boring_stuff_python/projects/comma_code.py
@@ -0,0 +1,5 @@
+spam = ['apples', 'bananas', 'tofu', 'cats']
+
+spam = (spam[0] + ', ' + spam[1] + ', ' + spam[2] + ', and ' + spam[3])
+
+print(spam)

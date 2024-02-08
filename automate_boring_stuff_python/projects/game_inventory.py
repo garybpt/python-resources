7913ee203b5e29d561999dc18a2d1d2cf758fc7c
Gary Butterfield <gary.butterfield@joyjunction.co.uk>
2024-02-08 16:48:51 +0000

    Still working through chapter 5 projects.

--- automate_boring_stuff_python/projects/game_inventory.py
+++ automate_boring_stuff_python/projects/game_inventory.py
@@ -1,15 +1,15 @@
+# A script to manage a player's game inventory
 
 
 player_inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
+
 def display_inventory(inventory):
     print("Inventory:")
     item_total = 0
-    for k, v in inventory.items():
-        print(v, k)
-    
+    for k, v in inventory.items(): # Loop through each item in the inventory
+        print(str(v) + ' ' + k) # Print the quantity and name of each item
+        item_total += v  # Incrementing the item_total by the quantity of each item
     print('Total number of items: ' + str(item_total))
-
-
 
 display_inventory(player_inventory)

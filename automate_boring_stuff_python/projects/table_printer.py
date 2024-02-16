02288a308581445f6b31aced8528af121096cd07
Gary Butterfield <gary.butterfield@joyjunction.co.uk>
2024-02-16 19:09:00 +0000

    Started working on a practice project.

---
+++ automate_boring_stuff_python/projects/table_printer.py
@@ -0,0 +1,11 @@
+# Python3
+# A simple programme that takes a list of lists of strings and displays them in a well-organised table with each column right-justified
+
+table_data = [['apples', 'oranges', 'cherries', 'banana'],
+              ['Alice', 'Bob', 'Carol', 'David'],
+              ['dogs', 'cats', 'moose', 'goose']]
+
+def print_table(table_data):
+
+
+# print(table_data[0] + table_data[1] + table_data[2])

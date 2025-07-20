60c27e7a14ee97c6940d3a71e4178a8e139016d4
Gary Butterfield <gary.butterfield@joyjunction.co.uk>
2025-07-20 09:22:39 +0100

    Added files.

--- atbs3e/2_flow_control/vampire.py
+++ atbs3e/2_flow_control/vampire.py
@@ -5,7 +5,7 @@ if name == 'Alice':
     print('Hello, Alice')
 elif age < 12:
     print('You are not Alice, kiddo')
-elif age > 100:
-    print('You are not Alice, grannie.')
 elif age > 2000:
     print('Unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, grannie.')
